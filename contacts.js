d4b43bc570a7e793957aec2dff515c18539ccef8
AlexBelozertsev <kashim983@gmail.com>
2021-05-18 17:54:15 +0300

    tiny refactoring contact-functions

--- contacts.js
+++ contacts.js
@@ -1,14 +1,21 @@
-const fs = require('fs').promises
+const fs = require('fs/promises')
 const path = require('path');
 const shortid = require('shortid')
 
 const contactsPath = path.join(__dirname, '/db/contact.json')
 
+async function getContacts() {
+  const data = await fs.readFile(contactsPath, 'utf-8');
+  return JSON.parse(data)
+}
+
+function writeNewList(initialList, newList) {
+  return fs.writeFile(initialList, newList, 'utf-8', (err) => { if (err) console.error(err) })
+}
+
 async function listContacts() {
   try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    const contacts = JSON.parse(data);
-    console.table(contacts)  
+    console.table(await getContacts())  
   } catch (error) {
     console.log(error);
   }
@@ -16,8 +23,7 @@ async function listContacts() {
 
 async function getContactById(contactId) {
     try {
-        const data = await fs.readFile(contactsPath, 'utf-8');
-      const contacts = JSON.parse(data)
+      const contacts = await getContacts()
       const findedContact = contacts.find(contact => contact.id === contactId)
      console.log(findedContact);
   } catch (error) {
@@ -27,11 +33,10 @@ async function getContactById(contactId) {
 
 async function removeContact(contactId) {
     try {
-      const data = await fs.readFile(contactsPath, 'utf-8');
-      const contacts = JSON.parse(data)
+      const contacts = await getContacts()
       const filteredContacts = contacts.filter(contact => contact.id !== contactId)
       const contactsList = JSON.stringify(filteredContacts, null, '\t')
-      fs.writeFile(contactsPath, contactsList, 'utf-8', (err) => { if (err) console.error(err) })
+      writeNewList(contactsPath, contactsList)
       console.log(`contact with ID: ${contactId} deleted from Contact List`);
       listContacts()
   } catch (error) {
@@ -41,11 +46,10 @@ async function removeContact(contactId) {
 
 async function addContact(name, email, phone) {
     try {
-        const data = await fs.readFile(contactsPath, 'utf-8');
-        const contacts = JSON.parse(data)
-        const contactNew = { id: shortid.generate(), name, email, phone }
-        const contactsList = JSON.stringify([contactNew, ...contacts], null, '\t')
-      fs.writeFile(contactsPath, contactsList, 'utf-8', (err) => { if (err) console.error(err) })
+      const contacts = await getContacts()
+      const contactNew = { id: shortid.generate(), name, email, phone }
+      const contactsList = JSON.stringify([contactNew, ...contacts], null, '\t')
+      writeNewList(contactsPath, contactsList)
       console.log(`contact ${name} added to Contact List`);
       listContacts()
     } catch (error) {
